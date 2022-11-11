57b4e1f0a032a6c2703ddd34486b6b9ef6bbe637
av1roytman <aroytman@purdue.edu>
2022-11-11 16:52:37 -0500

    asda

--- app.py
+++ app.py
@@ -18,9 +18,10 @@ def webhook():
     data = request.get_json()
 
     # We don't want to reply to ourselves!
-    if data['name'] != 'Fuck Jon':
-        msg = '{}, you sent "{}".'.format(data['name'], data['text'])
-        sendMessage(os.getenv('GROUPME_BOT_ID'), msg)
+    # if data['name'] != 'Fuck Jon':
+    #     msg = '{}, you sent "{}".'.format(data['name'], data['text'])
+    #     sendMessage(os.getenv('GROUPME_BOT_ID'), msg)
+    sendMessage(os.getenv('GROUPME_BOT_ID'), 'Hi')
 
     return "ok", 200
 
