11cbad42aa9a2901cb60f9dd4ecaa89413ca0703
av1roytman <aroytman@purdue.edu>
2022-11-11 16:24:00 -0500

    maybe fix?

--- main.py
+++ main.py
@@ -24,7 +24,8 @@ def webhook():
     # We don't want to reply to ourselves!
     if data['name'] != 'Fuck Jon':
         msg = '{}, you sent "{}".'.format(data['name'], data['text'])
-        sendMessage(os.getenv('GROUPME_BOT_ID'), msg)
+        # sendMessage(os.getenv('GROUPME_BOT_ID'), msg)
+        sendMessage('dc4082fa5dea7d3a200cc69f08', msg)
 
     return "ok", 200
 
